4c6489b52b072244bb91355dc9798ddf65698c97
RakonaDev <juancajas1905@gmail.com>
2024-12-10 10:39:56 -0500

    Inicializando

--- src/credentials.ts
+++ src/credentials.ts
@@ -5,12 +5,12 @@ import {  Auth, getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8cH4HdIaCl_mXvPrwuvQxTkkVJlZ5M8w",
-  authDomain: "store-fc667.firebaseapp.com",
-  projectId: "store-fc667",
-  storageBucket: "store-fc667.firebasestorage.app",
-  messagingSenderId: "787352696778",
-  appId: "1:787352696778:web:87f55b9a5919e11c538133"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROYECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
